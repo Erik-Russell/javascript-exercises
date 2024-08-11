3d25d456b501077684df0a8c6d5d5822f817b6ec
Erik Russell <erik.z.russell@gmail.com>
2024-08-11 08:49:14 -0700

    edited excercise 05 and PASSED all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b) || arguments.length > 2) {
+        return 'ERROR';
+    };
 
+    let sum = 0;
+    for (i = Math.min(a, b); i < (Math.max(a, b) + 1); i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
