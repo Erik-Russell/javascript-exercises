e20482b308e8c703417c295580915c9e92e4328d
Erik Russell <erik.z.russell@gmail.com>
2024-08-11 06:47:46 -0700

    edited excercise 02 and PASSED all test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
 
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    let output = '';
+    let i = 0;
+
+    while (i < num) {
+        output = output + string;
+        i++;
+    }
+
+    return output
 };
 
 // Do not edit below this line
