a8a51aba55fa726574723ae244200162fcd0437c
Erik Russell <erik.z.russell@gmail.com>
2024-08-11 08:24:15 -0700

    edited excercise 04 and PASSED all test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function () {
+    let result = arguments[0];
+    for (i = 1; i < arguments.length + 2; i++) {
+        result = result.filter((word) => word !== arguments[i]);
+    };
+    return result;
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
