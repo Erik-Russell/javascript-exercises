9dd77a9fb144d199426e732db51a90aa2df557a2
Erik Russell <erik.z.russell@gmail.com>
2024-08-17 11:26:50 -0700

    finished 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arrLibrary) {
+    let titles = [];
+    for (let index = 0; index < arrLibrary.length; index++) {
+        titles.push(arrLibrary[index].title);
+    }
+    return titles;
 };
 
 // Do not edit below this line
