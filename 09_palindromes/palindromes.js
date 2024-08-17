c59aee3a64518390afdbd2ac289685a3bf2e97e3
Erik Russell <erik.z.russell@gmail.com>
2024-08-17 07:24:42 -0700

    finished 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const fwd = Array.from(str).filter(char => {
+        const charCode = char.charCodeAt(0);
+        return !(charCode >= 32 && charCode <= 47) &&
+            !(charCode >= 58 && charCode <= 64) &&
+            !(charCode >= 91 && charCode <= 96) &&
+            !(charCode >= 123 && charCode <= 126);
+    }).join('').toLowerCase();
 
+    const rev = Array.from(str).filter(char => {
+        const charCode = char.charCodeAt(0);
+        return !(charCode >= 32 && charCode <= 47) &&
+            !(charCode >= 58 && charCode <= 64) &&
+            !(charCode >= 91 && charCode <= 96) &&
+            !(charCode >= 123 && charCode <= 126);
+    }).reverse().join('').toLowerCase();
+    if (fwd === rev) {
+        return true;
+    } else {
+        return false;
+    };
 };
 
 // Do not edit below this line
