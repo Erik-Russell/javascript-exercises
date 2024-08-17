6652676f205c444987cadf09823640fd48deabe7
Erik Russell <erik.z.russell@gmail.com>
2024-08-17 09:45:32 -0700

    finished 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let n = Number(num);
 
+    let fNMinus2 = 1;
+
+    let fNMinus1 = 1;
+
+    let fN;
+
+    if (n < 0) { return "OOPS" };
+
+    if (n === 0) { return 0 };
+
+    if (n == 1) { return fNMinus2 };
+
+    if (n === 2) { return fNMinus1 };
+
+    if (n > 2) {
+        for (let index = 3; index <= n; index++) {
+            fN = fNMinus2 + fNMinus1;
+            fNMinus2 = fNMinus1;
+            fNMinus1 = fN;
+        }
+        return fN;
+    };
 };
 
 // Do not edit below this line
