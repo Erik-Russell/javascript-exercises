4764fc02cab9b25b440e7f95cf0ce1cd0abfaad8
Erik Russell <erik.z.russell@gmail.com>
2024-08-18 06:29:34 -0700

    finished 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    let arrayCopy = Array.from(array)
+    arrayCopy.forEach((element) => {
+        if (!(typeof element.yearOfDeath === 'number')) {
+            const d = new Date();
+            element.yearOfDeath = d.getFullYear();
+        }
+    })
 
+    let isOldest = arrayCopy.sort(function (a, b) {
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    }).reverse().pop();
+
+    return isOldest;
 };
 
 // Do not edit below this line
